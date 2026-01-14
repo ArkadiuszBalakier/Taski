18412acf37a050642c5fc8303f131813507c9fca
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-14 10:08:08 +0100

    feat: add WorkerAdmin , TaskAdmin in admin.py and auth_user as worker in settings.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import Worker, Task, Position, TaskType, Tag, Team, Project
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+    ("Extra Fields", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Extra Fields", {"fields": ("position",)}),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "is_completed", "project")
+    list_filter = ("priority", "deadline", "is_completed", "task_type", "project")
+    search_fields = ("name", "description")
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.register(Tag)
+admin.site.register(Team)
+admin.site.register(Project)
