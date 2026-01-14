7c3c8b0c550f5a4b798cb2b4df4e2c4939969a15
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-14 12:18:57 +0100

    feat: add index view and configure settings.py

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+Python
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import Task, Project, Worker
+
+
+@login_required
+def index(request):
+    num_tasks = Task.objects.count()
+    num_projects = Project.objects.count()
+    num_workers = Worker.objects.count()
+
+    user_tasks = Task.objects.filter(assignees=request.user).count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_projects": num_projects,
+        "num_workers": num_workers,
+        "user_tasks": user_tasks,
+    }
+
+    return render(request, "manager/index.html", context=context)
