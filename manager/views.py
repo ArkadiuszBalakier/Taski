665bfcbf2dadf8191579eb416f24a88a2b0e31a5
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-14 13:53:25 +0100

    feat: add login.html , navigation.html base logic

--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-Python
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Task, Project, Worker
