68ea5e10741e4e181c000f2ea889f08ad7bc22c1
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-15 13:11:18 +0100

    fix: replace variable name to fit context task_stats

--- manager/context_processors.py
+++ manager/context_processors.py
@@ -8,7 +8,7 @@ def task_stats(request):
         return {
             "user_tasks_num": user_tasks.count(),
             "user_urgent_num": user_tasks.filter(priority="Urgent").count(),
-            "user_project_num": Project.objects.filter(teams__members=request.user)
+            "user_projects_num": Project.objects.filter(teams__members=request.user)
             .distinct()
             .count(),
             "user_tasks_names": user_tasks.values_list("name", flat=True),
