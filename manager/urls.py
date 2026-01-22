69e41aba0e75cffba2212d7c94bd70bbe02374bc
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-01-22 09:35:49 +0100

    feat: add tags urls

--- manager/urls.py
+++ manager/urls.py
@@ -21,6 +21,10 @@ from .views import (
     TaskTypeCreateView,
     TaskTypeUpdateView,
     TaskTypeDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 app_name = "manager"
@@ -62,4 +66,8 @@ urlpatterns = [
     ),
     path("workers/create", WorkerCreateView.as_view(), name="worker-create"),
     path("workers/<int:pk>/delete", WorkerDeleteView.as_view(), name="worker-delete"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
+    path("tags/create", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
